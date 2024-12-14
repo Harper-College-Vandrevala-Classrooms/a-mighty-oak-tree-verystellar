79b635f6ed990874435985dd6d43d1835e4e0981
verystellar <161178496+verystellar@users.noreply.github.com>
2024-12-13 19:46:50 -0600

    added tests

--- src/test/java/com/csc/TestOakTree.java
+++ src/test/java/com/csc/TestOakTree.java
@@ -16,6 +16,26 @@ public class TestOakTree {
 
   @Test
   void itWorks() {
-    assertEquals(true, true);
+    Squirrel cheeks = new Squirrel("Cheeks");
+    Node nodeOne = new Node(cheeks);
+
+    Squirrel squeaks = new Squirrel("Squeaks");
+    Node nodeTwo = new Node(squeaks);
+
+    Squirrel fluffybutt = new Squirrel("Mr. Fluffy Butt");
+    Node nodeThree = new Node(fluffybutt);
+
+    nodeOne.set_left(nodeTwo);
+    nodeOne.set_right(nodeThree);
+
+    Node retrievedLeft = nodeOne.left(); // This should retrieve the left node
+    Node retrievedRight = nodeOne.right(); // This should retrieve the right node
+    Node nodeFour = new Node(cheeks);
+
+    assertEquals(retrievedLeft.squirrel.getName(), squeaks.getName());
+    assertEquals(retrievedRight.squirrel.getName(), fluffybutt.getName());
+    assertEquals(nodeFour.left(), null);
+
+
   }
 }
